99cf162ff21089212307764fd75c8444b04e48bb
Serhat Tomak <serhattomak@gmail.com>
2023-12-05 11:42:32 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WFARoboticDrawing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.originalPictureBox = new System.Windows.Forms.PictureBox();
+            this.convertedPictureBox = new System.Windows.Forms.PictureBox();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.convertButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.originalPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.convertedPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // originalPictureBox
+            // 
+            this.originalPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.originalPictureBox.Name = "originalPictureBox";
+            this.originalPictureBox.Size = new System.Drawing.Size(500, 600);
+            this.originalPictureBox.TabIndex = 0;
+            this.originalPictureBox.TabStop = false;
+            // 
+            // convertedPictureBox
+            // 
+            this.convertedPictureBox.Location = new System.Drawing.Point(666, 12);
+            this.convertedPictureBox.Name = "convertedPictureBox";
+            this.convertedPictureBox.Size = new System.Drawing.Size(500, 600);
+            this.convertedPictureBox.TabIndex = 1;
+            this.convertedPictureBox.TabStop = false;
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(12, 618);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(400, 50);
+            this.loadButton.TabIndex = 2;
+            this.loadButton.Text = "Load Image";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(766, 618);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(400, 50);
+            this.convertButton.TabIndex = 3;
+            this.convertButton.Text = "Convert Image";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1178, 744);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.convertedPictureBox);
+            this.Controls.Add(this.originalPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.originalPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.convertedPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFARoboticDrawing
+{
+    public partial class Form1 : Form
+    {
+
+        private PictureBox originalPictureBox;
+        private PictureBox convertedPictureBox;
+        private Button loadButton;
+        private Button convertButton;
+        public Form1()
+        {
+            originalPictureBox=new PictureBox();
+            convertedPictureBox=new PictureBox();
+            loadButton=new Button();
+            convertButton=new Button();
+
+            loadButton.Click += new EventHandler(loadButton_Click);
+            convertButton.Click += new EventHandler(convertButton_Click);
+
+            Controls.Add(originalPictureBox);
+            Controls.Add(convertedPictureBox);
+            Controls.Add(loadButton);
+            Controls.Add(convertButton);
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog=new OpenFileDialog();
+            openFileDialog.Filter = "Image Files | *.jpg;*.jpeg;*.png;*.bmp;*.gif";
+
+            if (openFileDialog.ShowDialog()==DialogResult.OK)
+            {
+             Image image=LoadImage(openFileDialog.FileName);
+             originalPictureBox.Image= image;
+             originalPictureBox.SizeMode=PictureBoxSizeMode.StretchImage;
+            }
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            if (originalPictureBox.Image!=null)
+            {
+                Image convertedImage=Convert(originalPictureBox.Image);
+                Image drawingImage=ConvertToDrawing(convertedImage);
+                convertedPictureBox.Image= drawingImage;
+                convertedPictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else
+            {
+                MessageBox.Show("Please load an image first.");
+            }
+        }
+
+        private Image LoadImage(string path)
+        {
+            return Image.FromFile(path);
+        }
+
+        private Image Convert(Image originalImage)
+        {
+            Bitmap convertedImage = new Bitmap(originalImage.Width, originalImage.Height);
+
+            using (Graphics g=Graphics.FromImage(convertedImage))
+            {
+                ColorMatrix colorMatrix = new ColorMatrix(new float[][]
+                {
+                    new float[]{.3f,.3f,.3f,0,0},
+                    new float[]{.59f,.59f,.59f,0,0},
+                    new float[]{.11f,.11f,.11f,0,0},
+                    new float[]{0,0,0,1,0},
+                    new float[]{0,0,0,0,1}
+                });
+
+                using (ImageAttributes attributes=new ImageAttributes())
+                {
+                    attributes.SetColorMatrix(colorMatrix);
+                    g.DrawImage(originalImage, new Rectangle(0,0,originalImage.Width,originalImage.Height),0,0,originalImage.Width,originalImage.Height,GraphicsUnit.Pixel,attributes);
+                }
+            }
+            return convertedImage;
+        }
+
+        private Image ConvertToDrawing(Image convertedImage)
+        {
+            Bitmap drawingImage = new Bitmap(convertedImage.Width, convertedImage.Height);
+
+            using (Graphics g=Graphics.FromImage(drawingImage))
+            {
+                g.DrawImage(convertedImage,0,0);
+            }
+
+            return drawingImage;
+        }
+    }
+}
